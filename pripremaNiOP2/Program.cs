f67cb0463d7c29e1eb97f691fe10dc90b3dba2d5
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-11-14 19:32:11 +0100

    Add files via upload

---
+++ pripremaNiOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaNiOP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst, konacno;
+
+            Console.WriteLine("Napišite neku riječ: ");
+            tekst = Console.ReadLine();
+
+            if (tekst.Length <= 5)
+            {
+                Console.WriteLine(tekst);
+            }
+
+            else if (tekst.Length > 5)
+            {
+                konacno = tekst.Substring(2, tekst.Length - 5);
+
+                Console.WriteLine(konacno);
+            }
+
+            Console.ReadKey();
+        }   
+    }
+}
